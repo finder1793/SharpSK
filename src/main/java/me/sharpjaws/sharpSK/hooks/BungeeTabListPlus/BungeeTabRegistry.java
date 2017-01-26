38f04b7f2eb5f21571e7422e61d4e4f108772044
Sharpjaws <sharpjawsflex@gmail.com>
2017-01-26 16:38:11 +0100

    Adding more bungeetablist stuff.

--- src/main/java/me/sharpjaws/sharpSK/hooks/BungeeTabListPlus/BungeeTabRegistry.java
+++ src/main/java/me/sharpjaws/sharpSK/hooks/BungeeTabListPlus/BungeeTabRegistry.java
@@ -1,10 +1,13 @@
 package me.sharpjaws.sharpSK.hooks.BungeeTabListPlus;
+import org.bukkit.entity.Player;
+
+import codecrafter47.bungeetablistplus.api.bukkit.BungeeTabListPlusBukkitAPI;
+import codecrafter47.bungeetablistplus.api.bukkit.Variable;
+
 
 public class BungeeTabRegistry {
 
 	public static void registerBungeeTab(){
 		
-		
 	}
-	
 }
