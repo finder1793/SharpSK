caa1b32a202dcb83be852b88536990c20b12391f
Sharpjaws <sharpjawsflex@gmail.com>
2017-01-29 13:49:26 +0100

    Removed imports and delayed support for 1.6.2

--- src/main/java/me/sharpjaws/sharpSK/hooks/BungeeTabListPlus/BungeeTabRegistry.java
+++ src/main/java/me/sharpjaws/sharpSK/hooks/BungeeTabListPlus/BungeeTabRegistry.java
@@ -1,9 +1,4 @@
 package me.sharpjaws.sharpSK.hooks.BungeeTabListPlus;
-import org.bukkit.entity.Player;
-
-import codecrafter47.bungeetablistplus.api.bukkit.BungeeTabListPlusBukkitAPI;
-import codecrafter47.bungeetablistplus.api.bukkit.Variable;
-
 
 public class BungeeTabRegistry {
 
