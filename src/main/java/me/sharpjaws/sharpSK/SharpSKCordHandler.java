f7c88b9349397559b3773e9ab9fe84d920a475e9
Sharpjaws <sharpjawsflex@gmail.com>
2017-03-05 13:51:31 +0100

    Bungee stuff

---
+++ src/main/java/me/sharpjaws/sharpSK/SharpSKCordHandler.java
@@ -0,0 +1,26 @@
+package me.sharpjaws.sharpSK;
+
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.messaging.PluginMessageListener;
+
+public class SharpSKCordHandler extends main implements PluginMessageListener {
+@Override
+public void onEnable(){
+	 getServer().getMessenger().registerOutgoingPluginChannel(this, "SharpSKCord");
+	 getServer().getMessenger().registerIncomingPluginChannel(this, "SharpSKCord", this);
+}
+	
+
+	@Override
+	public void onPluginMessageReceived(String arg0, Player arg1, byte[] arg2) {
+	if (arg0.equals("SharpSKCord")){
+	
+		
+		
+	}else{
+		return;
+	}
+		
+	}
+
+}
