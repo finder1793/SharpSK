54b415a998380175c5d1162622bf86275f7be0a4
sharpjaws <sharpjawsflex@gmail.com>
2017-03-18 21:05:42 +0100

    Fixed more derps

--- src/main/java/me/sharpjaws/sharpSK/hooks/WorldGuard/WorldGuardRegistry.java
+++ src/main/java/me/sharpjaws/sharpSK/hooks/WorldGuard/WorldGuardRegistry.java
@@ -16,12 +16,11 @@ public class WorldGuardRegistry {
 	       Skript.registerEffect(EffRemoveOwner.class,  "[sharpsk] [(wg|worldguard)] remove owner %players/offlineplayers/strings% from wg region %string% in [world] %world%" );
 	       Skript.registerEffect(EffAddMember.class,  "[sharpsk] [(wg|worldguard)] add member[s] %players/offlineplayers/strings% to wg region %string% in [world] %world%" );
 	       Skript.registerEffect(EffRemoveMember.class,  "[sharpsk] [(wg|worldguard)] remove member[s] %players/offlineplayers/strings% from wg region %string% in [world] %world%" );
-	       Skript.registerExpression(ExprGetPoint1.class, Location.class, ExpressionType.SIMPLE, "[sharpsk] [wg|worldguard] (point|pos[ition])[ ] 1 of wg region %string% in [world] %world%");
-	       Skript.registerExpression(ExprGetPoint2.class, Location.class, ExpressionType.SIMPLE,"[sharpsk] [wg|worldguard] (point|pos[ition])[ ] 2 of wg region %string% in [world] %world%");
-	       Skript.registerExpression(ExprRegionAt.class, String.class, ExpressionType.SIMPLE, "[sharpsk] [wg|worldguard] wg region at %location%");
-	       Skript.registerExpression(ExprAllRegionsInWorld.class, String.class, ExpressionType.SIMPLE,  "[sharpsk] [wg|worldguard] [all] wg regions in %world%" );
+	       Skript.registerExpression(ExprGetPoint1.class, Location.class, ExpressionType.SIMPLE, "[sharpsk] [(wg|worldguard)] (point|pos[ition])[ ] 1 of wg region %string% in [world] %world%");
+	       Skript.registerExpression(ExprGetPoint2.class, Location.class, ExpressionType.SIMPLE,"[sharpsk] [(wg|worldguard)] (point|pos[ition])[ ] 2 of wg region %string% in [world] %world%");
+	       Skript.registerExpression(ExprRegionAt.class, String.class, ExpressionType.SIMPLE, "[sharpsk] [(wg|worldguard)] wg region at %location%");
+	       Skript.registerExpression(ExprAllRegionsInWorld.class, String.class, ExpressionType.SIMPLE,  "[sharpsk] [(wg|worldguard)] [all] wg regions in %world%" );
 	       Skript.registerEffect(EffSetBlocks.class, "[sharpsk] [(wg|worldguard)] (set [all] blocks in|fill) wg region %string% in [world] %world% (to|with) %itemstack%");
-	  
 			}else{
 				 Skript.registerEffect(EffCreateRegion.class,  "[(wg|worldguard)] create wg region %string% between %location% and %location% in [world] %world%" );
 			       Skript.registerEffect(EffDeleteRegion.class, "[(wg|worldguard)] (delete|remove) wg region %string% (in|from) [world] %world%" );
