b8e25116ff4d1084f0898343da6ee89bab26a664
sharpjaws <sharpjawsflex@gmail.com>
2017-03-18 21:23:55 +0100

    Added world searching to the syntax aswell

--- src/main/java/me/sharpjaws/sharpSK/hooks/WorldGuard/ExprAllMembers.java
+++ src/main/java/me/sharpjaws/sharpSK/hooks/WorldGuard/ExprAllMembers.java
@@ -3,7 +3,9 @@
  import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
+import org.bukkit.Bukkit;
 import org.bukkit.World;
 import org.bukkit.event.Event;
 
@@ -35,7 +37,18 @@ public boolean init(Expression<?>[] expressions, int i, Kleenean kleenean, Skrip
    }
    
    protected String[] get(Event event) {
-     World world = (World)this.world.getSingle(event);
+	   World world = (World)this.world.getSingle(event);
+	   if (world == null) {
+		   for (RegionManager a : WGBukkit.getPlugin().getRegionContainer().getLoaded()) {
+			 for  (Entry<String, ProtectedRegion> b :a.getRegions().entrySet()) {
+				if ( b.getKey().equals(region.getSingle(event))) {
+					world = Bukkit.getWorld(a.getName());
+					break;
+				}
+				 
+			 }
+		   }
+	   }
      
      RegionManager rm = WGBukkit.getRegionManager(world);
     ProtectedRegion pregion = rm.getRegion(region.getSingle(event));
