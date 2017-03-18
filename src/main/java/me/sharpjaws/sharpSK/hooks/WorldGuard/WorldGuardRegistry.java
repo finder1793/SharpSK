7c9fc119ea5f5f19a940a6cd2c1416f3f63faf1f
sharpjaws <sharpjawsflex@gmail.com>
2017-03-18 19:37:24 +0100

    Fixed another typo in the worldguard prefix for the syntax

--- src/main/java/me/sharpjaws/sharpSK/hooks/WorldGuard/WorldGuardRegistry.java
+++ src/main/java/me/sharpjaws/sharpSK/hooks/WorldGuard/WorldGuardRegistry.java
@@ -20,7 +20,7 @@ public class WorldGuardRegistry {
 	       Skript.registerExpression(ExprGetPoint2.class, Location.class, ExpressionType.SIMPLE,"[sharpsk] [wg|worldguard] (point|pos[ition])[ ] 2 of wg region %string% in [world] %world%");
 	       Skript.registerExpression(ExprRegionAt.class, String.class, ExpressionType.SIMPLE, "[sharpsk] [wg|worldguard] wg region at %location%");
 	       Skript.registerExpression(ExprAllRegionsInWorld.class, String.class, ExpressionType.SIMPLE,  "[sharpsk] [wg|worldguard] [all] wg regions in %world%" );
-	       Skript.registerEffect(EffSetBlocks.class, "[sharpsk] [wg|worldguard] (set [all] blocks in|fill) wg region %string% in [world] %world% (to|with) %itemstack%" );
+	       Skript.registerEffect(EffSetBlocks.class, "[sharpsk] [(wg|worldguard)] (set [all] blocks in|fill) wg region %string% in [world] %world% (to|with) %itemstack%");
 	  
 			}else{
 				 Skript.registerEffect(EffCreateRegion.class,  "[(wg|worldguard)] create wg region %string% between %location% and %location% in [world] %world%" );
