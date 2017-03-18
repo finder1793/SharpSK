811b41d2c76050a30a80a8404a827589f38e8cde
sharpjaws <sharpjawsflex@gmail.com>
2017-03-18 20:58:04 +0100

    Fixed NPE's. The region will now be searched for instead of a error.

--- src/main/java/me/sharpjaws/sharpSK/hooks/WorldGuard/EffAddOwner.java
+++ src/main/java/me/sharpjaws/sharpSK/hooks/WorldGuard/EffAddOwner.java
@@ -1,14 +1,17 @@
  package me.sharpjaws.sharpSK.hooks.WorldGuard;
  
- import org.bukkit.OfflinePlayer;
-import org.bukkit.World;
+ import java.util.Map.Entry;
 
+import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.World;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
 
 import com.sk89q.worldguard.bukkit.WGBukkit;
 import com.sk89q.worldguard.domains.DefaultDomain;
 import com.sk89q.worldguard.protection.managers.RegionManager;
+import com.sk89q.worldguard.protection.regions.ProtectedRegion;
 
 import ch.njol.skript.Skript;
 import ch.njol.skript.lang.Effect;
@@ -33,10 +36,21 @@ import ch.njol.util.Kleenean;
    
    protected void execute(Event event) {
 	   String name = (String)this.name.getSingle(event);
+	   World world = (World)this.world.getSingle(event);
+	   if (world == null) {
+		   for (RegionManager a : WGBukkit.getPlugin().getRegionContainer().getLoaded()) {
+			 for  (Entry<String, ProtectedRegion> b :a.getRegions().entrySet()) {
+				if ( b.getKey().equals(name)) {
+					world = Bukkit.getWorld(a.getName());
+					break;
+				}
+				 
+			 }
+		   }
 
-    World world = (World)this.world.getSingle(event);
-     
+	   }
     RegionManager regionManager = WGBukkit.getRegionManager(world);
+
     if (!regionManager.hasRegion(name)) {
       Skript.error("Region \"" + name + "\" in world \"" + world.getName() + "\" does not exists.");
       return;
--- src/main/java/me/sharpjaws/sharpSK/hooks/WorldGuard/EffRemoveMember.java
+++ src/main/java/me/sharpjaws/sharpSK/hooks/WorldGuard/EffRemoveMember.java
@@ -1,6 +1,9 @@
  package me.sharpjaws.sharpSK.hooks.WorldGuard;
  
- import org.bukkit.OfflinePlayer;
+ import java.util.Map.Entry;
+
+import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.World;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
@@ -8,6 +11,7 @@ import org.bukkit.event.Event;
 import com.sk89q.worldguard.bukkit.WGBukkit;
 import com.sk89q.worldguard.domains.DefaultDomain;
 import com.sk89q.worldguard.protection.managers.RegionManager;
+import com.sk89q.worldguard.protection.regions.ProtectedRegion;
 
 import ch.njol.skript.Skript;
 import ch.njol.skript.lang.Effect;
@@ -32,7 +36,19 @@ import ch.njol.util.Kleenean;
    
    protected void execute(Event event) {
      String name = (String)this.name.getSingle(event);
-     World world = (World)this.world.getSingle(event);
+	   World world = (World)this.world.getSingle(event);
+	   if (world == null) {
+		   for (RegionManager a : WGBukkit.getPlugin().getRegionContainer().getLoaded()) {
+			 for  (Entry<String, ProtectedRegion> b :a.getRegions().entrySet()) {
+				if ( b.getKey().equals(name)) {
+					world = Bukkit.getWorld(a.getName());
+					break;
+				}
+				 
+			 }
+		   }
+
+	   }
      
      RegionManager regionManager = WGBukkit.getRegionManager(world);
      if (!regionManager.hasRegion(name)) {
