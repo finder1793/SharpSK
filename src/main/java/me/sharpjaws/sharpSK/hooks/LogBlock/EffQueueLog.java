1caf15590a22a8f5bd33fb1b82a5ccb601201bc8
sharpjaws <sharpjawsflex@gmail.com>
2017-03-19 12:31:26 +0100

    Removing clutter and finalizing code

--- src/main/java/me/sharpjaws/sharpSK/hooks/LogBlock/EffQueueLog.java
+++ src/main/java/me/sharpjaws/sharpSK/hooks/LogBlock/EffQueueLog.java
@@ -5,16 +5,15 @@ import java.sql.SQLException;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.block.Block;
-import org.bukkit.block.BlockState;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
+import org.bukkit.inventory.ItemStack;
 import org.eclipse.jdt.annotation.Nullable;
 
 import ch.njol.skript.lang.Effect;
 import ch.njol.skript.lang.Expression;
 import ch.njol.skript.lang.SkriptParser.ParseResult;
 import ch.njol.util.Kleenean;
-import de.diddiz.LogBlock.Actor;
 import de.diddiz.LogBlock.BlockChange;
 import de.diddiz.LogBlock.Consumer;
 import de.diddiz.LogBlock.LogBlock;
@@ -24,17 +23,17 @@ import de.diddiz.LogBlock.QueryParams.BlockChangeType;
 public class EffQueueLog extends Effect{
 	private Expression<Player> player;
 	private Expression<Location> loc;
-	private Expression<Block> previ;
-	private Expression<Block> newi;
-	private int mark;
+	private Expression<ItemStack> previ;
+	private Expression<ItemStack> newi;
+	private int markr;
 	@SuppressWarnings("unchecked")
 	@Override
-	public boolean init(Expression<?>[] expr, int mark, Kleenean arg2, ParseResult result) {
+	public boolean init(Expression<?>[] expr, int pat, Kleenean arg2, ParseResult result) {
 		player = (Expression<Player>) expr[0];
 		loc = (Expression<Location>) expr[1];
-		previ = (Expression<Block>)expr[2];
-		newi = (Expression<Block>)expr[3];
-		mark = result.mark;
+		previ = (Expression<ItemStack>)expr[2];
+		newi = (Expression<ItemStack>)expr[3];
+		markr = result.mark;
 		return true;
 		
 	}
@@ -44,46 +43,47 @@ public class EffQueueLog extends Effect{
 		return "logblock queue block (break|place|replace) from %player% at %location% [with [previous block %-itemstack% and new] block %-itemstack%] ";
 	}
 
+	@SuppressWarnings("deprecation")
 	@Override
 	protected void execute(Event e) {
 		
 		LogBlock logblock = (LogBlock)Bukkit.getServer().getPluginManager().getPlugin("LogBlock");	
 		Consumer con  = logblock.getConsumer();
-		Actor a = Actor.actorFromEntity(player.getSingle(e));
-		
+		String a = player.getSingle(e).getName();
 	
-if (mark == 1) {
-con.queueBlockBreak(a, loc.getSingle(e).getBlock().getState());
-}else if (mark == 2) {
-	if (newi == null && previ == null) {
-con.queueBlockPlace(a, loc.getSingle(e).getBlock().getState());
+if (markr == 1) {
+con.queueBlockBreak(a, loc.getSingle(e), loc.getSingle(e).getBlock().getTypeId(),loc.getSingle(e).getBlock().getData());
+}else if (markr == 2) {
+con.queueBlockPlace(a, loc.getSingle(e), loc.getSingle(e).getBlock().getTypeId(),loc.getSingle(e).getBlock().getData());
+}else if (markr == 3) {
+	if (previ != null & newi != null) {
+		con.queueBlockReplace(a, loc.getSingle(e),previ.getSingle(e).getType().getId(),previ.getSingle(e).getData().getData(), newi.getSingle(e).getTypeId(), newi.getSingle(e).getData().getData());
 	}else {
-con.queueBlockPlace(a, newi.getSingle(e).getState());	
+		
+		QueryParams params = new QueryParams(logblock);
+		params.setPlayer(player.getSingle(e).getName());
+		params.bct = BlockChangeType.BOTH;
+		params.limit = -1;
+		params.world = loc.getSingle(e).getWorld();
+		params.needDate = true;
+		params.needType = true;
+		params.needData = true;
+		params.needPlayer = true;
+		int oldstate= 0;
+		byte oldstate2 = 0;
+		try {
+		    for (BlockChange bc : logblock.getBlockChanges(params)) {
+		    oldstate =  bc.type;
+		    oldstate2 =  bc.data;
+		    break;
+		    }
+		} catch (SQLException ex) {
+
+		}
+		con.queueBlockReplace(a, loc.getSingle(e), oldstate, oldstate2, loc.getSingle(e).getBlock().getTypeId(), loc.getSingle(e).getBlock().getData());
+	
 	}
-	
-}else if (mark == 3){
-	if (newi == null && previ == null) {
-	QueryParams params = new QueryParams(logblock);
-	params.setPlayer(player.getSingle(e).getName());
-	params.world = loc.getSingle(e).getWorld();
-	params.bct = BlockChangeType.BOTH;
-	BlockState newstate = null;
-	
-	try {
-		for (BlockChange bc : logblock.getBlockChanges(params)) {
-			newstate = bc.getLocation().getBlock().getState();		
-}
-	} catch (SQLException e1) {
-	
 	}
-	
-con.queueBlockReplace(a, loc.getSingle(e).getBlock().getState(), newstate);
-	
-}else {
-	con.queueBlockReplace(a, previ.getSingle(e).getState(), newi.getSingle(e).getState());
-}
-}	
-	
 	
 	}
 
--- src/main/java/me/sharpjaws/sharpSK/hooks/LogBlock/LogBlockRegistry.java
+++ src/main/java/me/sharpjaws/sharpSK/hooks/LogBlock/LogBlockRegistry.java
@@ -5,7 +5,7 @@ import ch.njol.skript.Skript;
 public class LogBlockRegistry {
 
 	public static void registerLogBlock() {
-		Skript.registerEffect(EffQueueLog.class, "logblock queue block (1在reak|2如lace|3字eplace) from %player% at %location% [with [previous block %-block% and new] block %-block%]");
+		Skript.registerEffect(EffQueueLog.class, "logblock queue block (1在reak|2如lace|3字eplace) from %player% at %location% [with previous block %-itemstack% and new block %-itemstack%]");
 	}
 	
 }
