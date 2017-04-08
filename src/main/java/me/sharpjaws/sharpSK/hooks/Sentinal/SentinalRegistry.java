f94e104c3ba713f092fe4a34886d8da7befe0793
sharpjaws <sharpjawsflex@gmail.com>
2017-04-08 17:39:15 +0200

    Added Sentinel Support

--- src/main/java/me/sharpjaws/sharpSK/hooks/Sentinal/SentinalRegistry.java
+++ src/main/java/me/sharpjaws/sharpSK/hooks/Sentinal/SentinalRegistry.java
@@ -1,8 +1,14 @@
 package me.sharpjaws.sharpSK.hooks.Sentinal;
 
+import org.mcmonkey.sentinel.events.SentinelAttackEvent;
+
+import ch.njol.skript.Skript;
+import ch.njol.skript.lang.util.SimpleEvent;
+
 public class SentinalRegistry {
 
 	public static void RegisterSentinal() {
+		Skript.registerEvent("Sentinel Target Event", SimpleEvent.class, SentinelAttackEvent.class, "[sentinel] target [event]");
 		
 	}
 	
