a29a5e48729475989591e1f4ec5be0bd072ddc92
sharpjaws <sharpjawsflex@gmail.com>
2017-05-14 13:45:06 +0200

    Fixed version check failing when there are strings after the version

--- src/main/java/me/sharpjaws/sharpSK/hooks/MythicMobs/MythicMobsRegistry.java
+++ src/main/java/me/sharpjaws/sharpSK/hooks/MythicMobs/MythicMobsRegistry.java
@@ -16,72 +16,14 @@ import me.sharpjaws.sharpSK.hooks.MythicMobs.old.CondIsMythicMobOld;
 import me.sharpjaws.sharpSK.hooks.MythicMobs.old.CondNotMythicMobOld;
 import me.sharpjaws.sharpSK.hooks.MythicMobs.old.EffSpawnMMOld;
 import me.sharpjaws.sharpSK.hooks.MythicMobs.old.ExprEvtMMDropsOld;
+import me.sharpjaws.sharpSK.hooks.MythicMobs.old.MythicMobsRegistryOld;
 
 public class MythicMobsRegistry {
 
 	public static void RegisterMythicMobs(){
-		if (!Bukkit.getPluginManager().getPlugin("MythicMobs").getDescription().getVersion().matches("4.\\d.\\d")) {
-		Skript.registerEvent("Mythicmob death", SimpleEvent.class, net.elseland.xikage.MythicMobs.API.Bukkit.Events.MythicMobDeathEvent.class,"([mythicmob|mm]) death");
-		EventValues.registerEventValue( net.elseland.xikage.MythicMobs.API.Bukkit.Events.MythicMobDeathEvent.class, Location.class,
-				new Getter<Location, net.elseland.xikage.MythicMobs.API.Bukkit.Events.MythicMobDeathEvent>() {
-					@Override
-					@Nullable
-					public Location get(net.elseland.xikage.MythicMobs.API.Bukkit.Events.MythicMobDeathEvent e) {
-						Location l = e.getEntity().getLocation();
-						return l;
-					}
-				}, 0);
-		EventValues.registerEventValue(net.elseland.xikage.MythicMobs.API.Bukkit.Events.MythicMobDeathEvent.class, String.class,
-				new Getter<String,net.elseland.xikage.MythicMobs.API.Bukkit.Events.MythicMobDeathEvent>() {
-					@Override
-					@Nullable
-					public String get(net.elseland.xikage.MythicMobs.API.Bukkit.Events.MythicMobDeathEvent e) {
-						String l = e.getMobType().getInternalName();
-						return l;
-					}
-				}, 0);
-		Skript.registerEvent("Mythicmob spawn", SimpleEvent.class, net.elseland.xikage.MythicMobs.API.Bukkit.Events.MythicMobSpawnEvent.class,"([mythicmob|mm]) spawn");
-		EventValues.registerEventValue(net.elseland.xikage.MythicMobs.API.Bukkit.Events.MythicMobSpawnEvent.class, Entity.class,
-				new Getter<Entity, net.elseland.xikage.MythicMobs.API.Bukkit.Events.MythicMobSpawnEvent>() {
-					@Override
-					@Nullable
-					public Entity get(net.elseland.xikage.MythicMobs.API.Bukkit.Events.MythicMobSpawnEvent e) {
-						Entity en = e.getEntity();
-						return en;
-					}
-				}, 0);
-		EventValues.registerEventValue(net.elseland.xikage.MythicMobs.API.Bukkit.Events.MythicMobSpawnEvent.class, String.class,
-				new Getter<String, net.elseland.xikage.MythicMobs.API.Bukkit.Events.MythicMobSpawnEvent>() {
-					@Override
-					@Nullable
-					public String get(net.elseland.xikage.MythicMobs.API.Bukkit.Events.MythicMobSpawnEvent e) {
-						String name = e.getMobType().getInternalName();
-						return name;
-					}
-				}, 0);
-		Skript.registerExpression(ExprEvtMMDropsOld.class, ItemStack.class, ExpressionType.SIMPLE, "[all] [event-]mmdrops");
-		Skript.registerCondition(CondIsMythicMobOld.class, "%entity% is a mythicmob");
-		Skript.registerCondition(CondNotMythicMobOld.class, "%entity% is not a mythicmob");
-		Skript.registerEffect(EffSpawnMMOld.class, "[sharpsk] spawn [a] mythicmob %string% at [the] %location% with level %integer%");
-		Skript.registerEvent("Mythicmob Skill", SimpleEvent.class, net.elseland.xikage.MythicMobs.API.Bukkit.Events.MythicMobSkillEvent.class,"([mythicmob|mm]) skill [cast]");
-		EventValues.registerEventValue(net.elseland.xikage.MythicMobs.API.Bukkit.Events.MythicMobSkillEvent.class, Location.class,
-				new Getter<Location, net.elseland.xikage.MythicMobs.API.Bukkit.Events.MythicMobSkillEvent>() {
-					@Override
-					@Nullable
-					public Location get(net.elseland.xikage.MythicMobs.API.Bukkit.Events.MythicMobSkillEvent e) {
-						Location l = e.getEntity().getLocation();
-						return l;
-					}
-				}, 0);
-		EventValues.registerEventValue(net.elseland.xikage.MythicMobs.API.Bukkit.Events.MythicMobSkillEvent.class, String.class,
-				new Getter<String, net.elseland.xikage.MythicMobs.API.Bukkit.Events.MythicMobSkillEvent>() {
-					@Override
-					@Nullable
-					public String get(net.elseland.xikage.MythicMobs.API.Bukkit.Events.MythicMobSkillEvent e) {
-						String n = e.getSkillName();
-						return n;
-					}
-				}, 0);
+		if (!Bukkit.getPluginManager().getPlugin("MythicMobs").getDescription().getVersion().matches("4.\\d.\\d.*")) {
+			System.out.println("Seems like the version matched");
+		MythicMobsRegistryOld.RegisterMythicMobsOld();
 		}else{
 		Skript.registerEvent("Mythicmob death", SimpleEvent.class, io.lumine.xikage.mythicmobs.api.bukkit.events.MythicMobDeathEvent.class,"([mythicmob|mm]) death");
 		EventValues.registerEventValue(io.lumine.xikage.mythicmobs.api.bukkit.events.MythicMobDeathEvent.class, Location.class,
