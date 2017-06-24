77cb1110acb565a042094ea90c6c44a238a6b03b
sharpjaws <sharpjawsflex@gmail.com>
2017-06-24 10:55:16 +0200

    Changed the syntax up a bit

--- src/main/java/me/sharpjaws/sharpSK/hooks/CoreProtect/CorePRegistry.java
+++ src/main/java/me/sharpjaws/sharpSK/hooks/CoreProtect/CorePRegistry.java
@@ -7,7 +7,7 @@ public class CorePRegistry {
 	public static void registerCoreP(){
 		Skript.registerEffect(EffCoreRollback.class, "[(coreprotect|cp)] (rollback|revert|undo) [(0¦removal|1¦placement|2¦interaction)] changes [made by %-offlineplayers%] at [the] %location% in radius %number% [back] to %timespan% [ago] [and] [exclude %-itemstacks%]");
 		Skript.registerEffect(EffCoreRestore.class, "[(coreprotect|cp)] restore [(0¦removal|1¦placement|2¦interaction)] [rollback] changes [made by %-offlineplayers%] at [the] %location% in radius %number% from %timespan% [ago] [and] [exclude %-itemstacks%]");
-		Skript.registerEffect(EffCoreLog.class, "[(coreprotect|cp)] log block [(0¦removal|1¦placement|2¦interaction)] at %location% for %offlineplayer%");
+		Skript.registerEffect(EffCoreLog.class, "[(coreprotect|cp)] log block [(0¦removal|1¦placement|2¦interaction)] at %location% (for|by) %offlineplayer%");
 	}
 	
 }
